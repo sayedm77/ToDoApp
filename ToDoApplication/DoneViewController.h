a87401a3567aa57a93f327a874a9e9a2953d7de2
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-08-12 17:55:41 +0300

    modify the done screen and submit the userdefault

--- ToDoApplication/DoneViewController.h
+++ ToDoApplication/DoneViewController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface DoneViewController : UIViewController
+@interface DoneViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
 
 @end
 
--- ToDoApplication/DoneViewController.m
+++ ToDoApplication/DoneViewController.m
@@ -6,26 +6,143 @@
 //
 
 #import "DoneViewController.h"
+#import "Task.h"
+#import "DetailsViewController.h"
 
 @interface DoneViewController ()
-
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray<Task*> *todoArray;
+@property NSMutableArray<Task*> *filterArray;
 @end
 
 @implementation DoneViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.tabBarController.navigationItem.title = @"Done";
+    self.tabBarController.navigationItem.rightBarButtonItem.hidden = TRUE;
+    [self loadTasks];
+    [self filterTasks];
+    [self checkTableOrImage];
+    [_tableView reloadData];
 }
-*/
+
+-(void)loadTasks{
+    NSError * error;
+    NSData * savedData = [[NSUserDefaults standardUserDefaults] objectForKey:@"tasks"];
+    if(savedData!= nil){
+        NSSet * set = [NSSet setWithArray:@[[NSMutableArray class],[Task class]]];
+        _todoArray = (NSMutableArray*) [NSKeyedUnarchiver unarchivedObjectOfClasses:set fromData:savedData error:&error];
+    }
+}
+-(void)checkTableOrImage{
+    if(_filterArray.count == 0){
+        _tableView.hidden = TRUE;
+        _imgView.hidden = FALSE;
+        _imgView.image = [UIImage imageNamed:@"done"];
+    } else {
+        _tableView.hidden = FALSE;
+        _imgView.hidden = TRUE;
+    }
+}
+
+- (void)filterTasks {
+    _filterArray = [NSMutableArray new];
+    for (Task *task in _todoArray) {
+        if (task.type == 2) {
+            [_filterArray addObject:task];
+        }
+    }
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _filterArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [_tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    switch (_filterArray[indexPath.row].priority) {
+        case 0:
+            cell.imageView.image = ([[UIImage imageNamed:@"low"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]);
+            break;
+        case 1:
+            cell.imageView.image = ([[UIImage imageNamed:@"medium"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]);
+            break;
+        case 2:
+            cell.imageView.image = ([[UIImage imageNamed:@"high"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]);
+            break;
+            
+        default:
+            break;
+    }
+    cell.imageView.tintColor = [UIColor systemIndigoColor];
+    cell.textLabel.text = _filterArray[indexPath.row].title;
+    
+    
+    return cell;
+    
+}
+
+- (UISwipeActionsConfiguration *)tableView:(UITableView *)tableView trailingSwipeActionsConfigurationForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    Task *task = _filterArray[indexPath.row];
+    
+    UIContextualAction *editAction = [UIContextualAction contextualActionWithStyle:UIContextualActionStyleNormal title:@"Edit"
+        handler:^(UIContextualAction *action, UIView *sourceView, void (^completionHandler)(BOOL)) {
+        [self editTask:task atIndex:indexPath.row];
+        completionHandler(YES);
+    }];
+    editAction.backgroundColor = [UIColor systemMintColor];
+    
+    UIContextualAction *deleteAction = [UIContextualAction contextualActionWithStyle:UIContextualActionStyleDestructive title:@"Delete"
+        handler:^(UIContextualAction *action, UIView *sourceView, void (^completionHandler)(BOOL)) {
+        [self deleteTask:task atIndex:indexPath.row];
+    }];
+    deleteAction.backgroundColor = [UIColor systemRedColor];
+    UISwipeActionsConfiguration *actions = [UISwipeActionsConfiguration configurationWithActions:@[editAction, deleteAction]];
+    return actions;
+}
+
+- (void)editTask:(Task *)task atIndex:(NSInteger)index {
+    int todoIndex = (int)[_todoArray indexOfObject:task];
+    DetailsViewController * detailsVC = [self.storyboard instantiateViewControllerWithIdentifier:@"Details"];
+    detailsVC.editingTask = task;
+    detailsVC.editing = TRUE;
+    detailsVC.objectIndex = todoIndex;
+    detailsVC.pusher = @"done";
+    [self.navigationController pushViewController:detailsVC animated:TRUE];
+}
+
+- (void)deleteTask:(Task *)task atIndex:(NSInteger)index {
+    int todoIndex = (int)[_todoArray indexOfObject:task];
+    [_todoArray removeObjectAtIndex:todoIndex];
+    [self saveTasks];
+    [self filterTasks];
+    [self checkTableOrImage];
+    [_tableView reloadData];
+}
+
+- (void)saveTasks {
+    NSError *error = nil;
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:_todoArray requiringSecureCoding:NO error:&error];
+    [[NSUserDefaults standardUserDefaults] setObject:data forKey:@"tasks"];
+    
+}
+
 
 @end
